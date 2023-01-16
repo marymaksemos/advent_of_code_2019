a8d7ee1e9af3987f2ce3e6327460fb83f4301ae4
marymaksemos <117455640+marymaksemos@users.noreply.github.com>
2023-01-16 11:51:44 +0100

    the test of the second function of day2 part 2

--- test_day_2_part_2.py
+++ test_day_2_part_2.py
@@ -16,3 +16,9 @@ def test_intcode():
     program = [1,1,1,4,99,5,6,0,99]
     result = intcode(program)
     assert result == [30,1,1,4,2,5,6,0,99]
+
+def test_calculate_output():
+    program = [2,3,0,3,99,4,5,2,3,42,1,2,1]
+    y=2
+    result = calculate_output(program,y) 
+    assert result == 0
