cbe89eaa1dd74ccd062a57693c1ec31dfed4dc1f
marymaksemos <117455640+marymaksemos@users.noreply.github.com>
2023-01-16 11:08:28 +0100

    the main function

--- day_1_part_1.py
+++ day_1_part_1.py
@@ -4,4 +4,13 @@ def fuel_required(mass) :
 
 def total_fuel_required(masses) :
      return sum(fuel_required(mass) for mass in masses)
-  
+
+
+if __name__ == '__main__':
+ masses=[]
+ with open("input_day_1part_1.txt")as file:
+        for line in file:
+            masses.append(int(line.strip()))
+
+ print(f"result : {total_fuel_required(masses)}")
+
