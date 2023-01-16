dcf739ece710c0095358f83f1fcfcfaf4c499bd7
marymaksemos <117455640+marymaksemos@users.noreply.github.com>
2023-01-16 11:05:55 +0100

    the test of function total_fuel_required

--- test_day_1_part_1.py
+++ test_day_1_part_1.py
@@ -1,11 +1,14 @@
 import unittest
-from day_1_part_1 import fuel_required
+from day_1_part_1 import fuel_required,total_fuel_required
 class TestFuelCalculations(unittest.TestCase):
     def test_fuel_required(self):
         self.assertEqual(fuel_required(12), 2)
         self.assertEqual(fuel_required(14), 2)
         self.assertEqual(fuel_required(1969), 654)
         self.assertEqual(fuel_required(100756), 33583)
+        
+    def test_total_fuel_required(self):
+        self.assertEqual(total_fuel_required([12, 14, 1969, 100756]), 34241)
 
 
 if __name__ == '__main__':
